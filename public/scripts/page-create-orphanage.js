c2f896ea916a052a7e6e99f33f77a57ebf97f6e3
Victor Pantoja <victor.pko@outlook.com>
2021-05-13 23:35:22 -0300

    atualizacao-002
    Aula 3 terminada!
    Parte Javascript do formulário criada.
    1 - Possível inserir ou remover fotos e o que estiver no campo de fotos
    2 - Botões (sim) e (não) funcionando também.

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -22,7 +22,6 @@ map.on('click', (event) => {
    document.querySelector('[name=lat]').value = lat;
    document.querySelector('[name=lng]').value = lng;
 
-
    //remove icon
    marker && map.removeLayer(marker)
 
@@ -36,10 +35,51 @@ function addPhotoField() {
    // pegar o container de fotos #images
    const container = document.querySelector('#images')
    // pegar o container para duplicar .new-upload
-   const fieldsContainer = document.querySelectorAll('.new-image')
+   const fieldsContainer = document.querySelectorAll('.new-upload')
    // realizar o clone da ultima imagem adicionada
-   const newfieldContainer = fieldsContainer[fieldsContainer.length -1].cloneNode(true)
+   const newFieldContainer = fieldsContainer[fieldsContainer.length -1].cloneNode(true)
+   // verificar se o campo está vasio, se sim, não adicionar ao container de imagens
+   const input = newFieldContainer.children[0]
+   if (input.value == "") {
+      return
+   }
+   // limpar o campo antes de adicionar ao container de imagens
+   input.value = ""
    // adicionar o clone ao container de #images
-   container.appendChild(newfieldContainer)
+   container.appendChild(newFieldContainer)
 }
 
+function deleteField(event) {
+   const span = event.currentTarget
+
+   const fieldsContainer = document.querySelectorAll('.new-upload')
+
+   if (fieldsContainer.length < 2) {
+      // limpar o valor do campo
+      span.parentNode.children[0].value = ""
+      return
+   }
+   // deletar o campo
+   span.parentNode.remove()
+}
+
+// select yes or no
+function toggleSelect(event) {
+
+   // retirar a class .active (dos botoes)
+   document.querySelectorAll('.button-select button')
+   .forEach( function(button) {
+      button.classList.remove('active')
+   })
+
+   // colocar a class .active nesse botao clicado
+   const button = event.currentTarget
+   button.classList.add('active')
+
+   // atualizar o meu input hidden com o valor selecionado
+   const input = document.querySelector('[name="open_on_weekends"]')
+
+   input.value = button.dataset.value
+}
+
+// Aula 04 : Começar
