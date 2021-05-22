0e37c68cf38640aff0faaa6bdd2bcef6cfe7cc8e
Victor Pantoja <victor.pko@outlook.com>
2021-05-21 21:05:18 -0300

    atualizacao-005
    Aula 4 finalizada.
    Mais um pouco de back-end
    Um arquivo de dados fake criado para testar requisição e resposta da pagina orphanages.

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+   {
+      id: 1,
+      lat: "-22.7605206",
+      lng: "-47.4155656",
+      name: "Lar das meninas",
+      description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+      images: [
+         "https://images.unsplash.com/photo-1618785572542-09eea56869b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxNTU5MTIw&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+         "https://images.unsplash.com/photo-1617758387451-79bcae2d86eb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxNTU5MjA3&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+      ],
+      instructions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+      opening_hours: "Horário de visitas Das 18h até 8h",
+      open_on_weekends: "1"
+   },
+
+   {
+      id: 2,
+      lat: "-22.7605206",
+      lng: "-47.4195656",
+      name: "Lar dos meninos",
+      description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+      images: [
+         "https://images.unsplash.com/photo-1618785572542-09eea56869b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxNTU5MTIw&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+         "https://images.unsplash.com/photo-1617758387451-79bcae2d86eb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIxNTU5MjA3&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+      ],
+      instructions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+      opening_hours: "Horário de visitas Das 18h até 8h",
+      open_on_weekends: "0"
+   }
+]
